07248ce8f2b095254e4eb9807fceb070d58df613
Colin Durbin <cddurbin@gmail.com>
2015-02-11 03:57:50 +0000

    Made game creat action and form for selecting who to play.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,17 +1,34 @@
 class GamesController < ApplicationController
+  before_action :authenticate_user!
 
   def index
   end
 
-  def show
-  end
-
   def new
+    @game = Game.new
   end
 
   def create
+    @game = Game.new(game_params)
+
+    if @game.save
+      binding.pry
+      nil
+      
+      redirect_to @game
+    else
+      render 'new'
+    end
+
   end
 
+  def show
+    @game = Game.find(params[:id])
+  end
 
+  private
+  def game_params
+    params.require(:game).permit(:winner, :completed, :player1_id, :player2_id)
+  end
 
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,11 +1,20 @@
 class Game < ActiveRecord::Base
-
+  attr_accessor :current_user
   has_many :moves
   belongs_to :player1, class_name: 'User'
   belongs_to :player2, class_name: 'User'
 
-  def start
-    Board.new
-  end
+  # def start
+  #   @board = Board.new
+  # end
+
+
+  # def player2_assignment(player2)
+  #   self.player2_id = player2
+  # end
+
+
+
+
 
 end
