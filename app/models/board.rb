d88533eb1bf001d610c8f2f7e055b5324347d3a7
Colin Durbin <cddurbin@gmail.com>
2015-02-11 02:19:42 +0000

    Created controllers and views for games and moves.

---
+++ app/models/board.rb
@@ -0,0 +1,14 @@
+class Board
+  attr_accessor :fresh
+
+  def initialize
+     @fresh = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  end
+
+  def remove_from_available(player_choice)
+    @fresh -= [player_choice]
+  end
+
+
+
+end
--- app/models/game.rb
+++ app/models/game.rb
@@ -4,4 +4,8 @@ class Game < ActiveRecord::Base
   belongs_to :player1, class_name: 'User'
   belongs_to :player2, class_name: 'User'
 
+  def start
+    Board.new
+  end
+
 end
